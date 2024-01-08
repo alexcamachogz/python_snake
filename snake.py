e11f11a94a0f9c3d328d9afe6574d4bbc3d32d9b
Alejandra Camacho <camacho.ale94@gmail.com>
2024-01-08 16:39:56 -0600

    feat: Create Snake class to control all related with the snake

---
+++ snake.py
@@ -0,0 +1,55 @@
+from turtle import Turtle
+
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        snake = Turtle("square")
+        snake.color("white")
+        snake.penup()
+        snake.goto(position)
+        self.segments.append(snake)
+
+    def reset(self):
+        for segment in self.segments:
+            segment.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def extends(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        len_segments = len(self.segments) - 1
+        for index in range(len_segments, 0, -1):
+            new_x = self.segments[index - 1].xcor()
+            new_y = self.segments[index - 1].ycor()
+            self.segments[index].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def reset(self):
+        for segment in self.segments:
+            segment.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def extends(self):
+        self.add_segment(self.segments[-1].position())
+    
